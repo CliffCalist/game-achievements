9f6a6a92ee1f818e38ba8c06b45c64e3cc6ef6fe
Cliff Calist <cliffstersprog@gmail.com>
2025-06-21 18:29:46 +0200

    feat(reward-config): implement icon and amount properties

--- Runtime/Achievement/Reward/RewardConfig.cs
+++ Runtime/Achievement/Reward/RewardConfig.cs
@@ -3,5 +3,17 @@ using UnityEngine;
 namespace WhiteArrow.GameAchievements
 {
     public abstract class RewardConfig : ScriptableObject
-    { }
+    {
+        [Header("Optional")]
+        [SerializeField] private Sprite _icon;
+
+        [Header("Required")]
+        [SerializeField, Min(0)] private int _amount = 1;
+
+
+
+        public Sprite Icon => _icon;
+
+        public int Amount => _amount;
+    }
 }
