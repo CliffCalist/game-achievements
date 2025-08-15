c3f9c942b5672510064f701c2f53f2e008a8728f
Cliff Calist <cliffstersprog@gmail.com>
2025-08-15 19:42:57 +0200

    refactor(reward-config): remove non-business data

--- Runtime/Achievement/Reward/AchievementRewardConfig.cs
+++ Runtime/Achievement/Reward/AchievementRewardConfig.cs
@@ -4,15 +4,9 @@ namespace WhiteArrow.GameAchievements
 {
     public abstract class AchievementRewardConfig : ScriptableObject
     {
-        [Header("Optional")]
-        [SerializeField] private Sprite _icon;
-
-        [Header("Required")]
         [SerializeField, Min(0)] private int _amount = 1;
 
 
-
-        public Sprite Icon => _icon;
 
         public int Amount => _amount;
     }
