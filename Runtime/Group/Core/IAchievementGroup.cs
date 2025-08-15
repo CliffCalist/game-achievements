a796f94bd7cfb99721cd474c33f92dd0bbb2c67b
Cliff Calist <cliffstersprog@gmail.com>
2025-08-16 00:03:56 +0200

    feat(group): add RestoreState and CaptureStateTo methods to IAchievementGroup

--- Runtime/Group/Core/IAchievementGroup.cs
+++ Runtime/Group/Core/IAchievementGroup.cs
@@ -7,6 +7,9 @@ namespace WhiteArrow.GameAchievements
         AchievementGroupConfig Config { get; }
         IReadOnlyCollection<Achievement> Achievements { get; }
 
+        void RestoreState(IAchievementGroupSnapshot snapshot);
+        IAchievementGroupSnapshot CaptureStateTo(IAchievementGroupSnapshot snapshot);
+
         void Init();
     }
 }
